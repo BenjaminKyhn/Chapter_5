51387150c69e89d7fc8993132c673ec82bb40ab7
Benjamin Kyhn <benj2605@edu.easj.dk>
2019-09-25 13:15:09 +0200

    Exercise 5.02 added. Changed the name of Exercise 5.1 to Exercise 5.01.

--- src/Exercise_5_01.java
+++ src/Exercise_5_01.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Exercise_5_1 {
+public class Exercise_5_01 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
