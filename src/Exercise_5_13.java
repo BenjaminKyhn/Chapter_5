d615254a59f864ae794fec20013d32bea4f62fb5
Benjamin Kyhn <benkyhn@hotmail.com>
2019-09-26 14:23:46 +0200

    Exercise 5.13 added.

---
+++ src/Exercise_5_13.java
@@ -0,0 +1,10 @@
+public class Exercise_5_13 {
+    public static void main(String[] args) {
+        int n = 1;
+        while (Math.pow(n, 2) < 12000){
+            n++;
+        }
+        System.out.println(n-1);
+
+    }
+}
