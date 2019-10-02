717011773a579df9903c79f4de42c20ae5a621ef
Benjamin Kyhn <benkyhn@hotmail.com>
2019-10-02 11:17:41 +0200

    Exercise 5.46 added

---
+++ src/Exercise_5_48.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Exercise_5_48 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String s = input.nextLine();
+
+        int count = 1;
+        while (count < s.length()){
+            System.out.print(s.charAt(count));
+            count = count+2;
+        }
+    }
+}
